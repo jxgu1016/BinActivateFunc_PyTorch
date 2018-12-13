43d13d5ba1d62b9f8c249bfe31894883e50c0f6f
Jiaxin Gu <jxgu1016@gmail.com>
2018-12-13 15:59:20 +0800

    update to 1.0

--- src/cuda/BinActivateFunc_cuda.cpp
+++ src/cuda/BinActivateFunc_cuda.cpp
@@ -10,8 +10,8 @@ int BinActivateFunc_cuda_backward(
 
 // C++ interface
 
-#define CHECK_CUDA(x) AT_ASSERT(x.type().is_cuda(), #x " must be a CUDA tensor")
-#define CHECK_CONTIGUOUS(x) AT_ASSERT(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_CUDA(x) AT_ASSERTM(x.type().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) AT_ASSERTM(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
 at::Tensor BinActivateFunc_forward(
